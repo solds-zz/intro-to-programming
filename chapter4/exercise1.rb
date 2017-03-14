3495eacf1bef12425ff1575535b5fa6815639a50
Skyler Olds <skylerolds@gmail.com>
2017-03-14 11:39:48 -0600

    finish chapter 4

---
+++ chapter4/exercise1.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  "Greetings, #{name}!"
+end
+
+puts greeting('Skyler')
---
+++ chapter4/exercise5.rb
@@ -0,0 +1,8 @@
+# Returns nil
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
