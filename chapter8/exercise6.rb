3b632e693d45bc7845318e56adad4621f938c2bc
Skyler Olds <skylerolds@gmail.com>
2017-03-16 07:42:34 -0600

    finish chapter 8

--- chapter8/exercise6.rb
+++ chapter8/exercise6.rb
@@ -1,3 +1,9 @@
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
+
+words_hash = words.each_with_object(Hash.new []) do |word, hash|
+  hash[word.chars.sort.join('')] += [word]
+end
+
+words_hash.each { |k, v| p v }
